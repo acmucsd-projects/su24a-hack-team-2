a0b9c238b70a7ccd5f55bed2cf0d24262d08423f
jonathannnty <141669529+jonathannnty@users.noreply.github.com>
2024-08-19 17:32:31 -0700

    bouncing logo and redesign!

--- source/js/user-page.js
+++ source/js/user-page.js
@@ -1,23 +1,67 @@
+// Hover over button, text-emoji changes to corresponding buttons
 const button_1 = document.querySelector("#button_1");
 const button_2 = document.querySelector("#button_2");
-const emoji_text = document.querySelector("#emoji");
+const emoji_text1 = document.querySelector("#emoji_1");
+const emoji_text2 = document.querySelector("#emoji_2");
 
-button_1.addEventListener("mouseover", () => {
-  emoji_text.textContent = "🗒️✍️";
-  emoji_text.classList.remove('fade-out')
-},1000);
+// Bouncing animation for Go Go Grocery Logo
+const logo = document.querySelector(".bouncing-logo");
+const container = document.querySelector("#user-details-emoji");
 
-button_1.addEventListener("mouseout", () => {
-  emoji_text.textContent = "";
-  emoji_text.classList.add('fade-out')
-},1000);
+// Initializing position and velocity
+let posX = 0;
+let posY = 0;
+let velocityX = 2;
+let velocityY = 2;
 
-button_2.addEventListener("mouseover", () => {
-  emoji_text.textContent = "📋🧐💭";
-  emoji_text.classList.remove('fade-out')
-},1000);
+function animate() {
+  posX += velocityX;
+  posY += velocityY;
+  if (posX + logo.clientWidth > container.clientWidth || posX < 0) {
+    velocityX = -velocityX;
+  }
+  if (posY + logo.clientHeight > container.clientHeight || posY < 0) {
+    velocityY = -velocityY;
+  }
+  logo.style.left = `${posX}px`;
+  logo.style.top = `${posY}px`;
+  requestAnimationFrame(animate);
+}
 
-button_2.addEventListener("mouseout", () => {
-  emoji_text.textContent = "";
-  emoji_text.classList.add('fade-out')
-},1000);
+animate();
+
+button_1.addEventListener(
+  "mouseover",
+  () => {
+    emoji_text1.textContent = "🗒️✍️";
+    emoji_text1.classList.remove("fade-out");
+  },
+  1000
+);
+
+button_1.addEventListener(
+  "mouseout",
+  () => {
+    emoji_text1.textContent = "&nbsp;";
+    emoji_text1.classList.add("fade-out");
+  },
+  1000
+);
+
+button_2.addEventListener(
+  "mouseover",
+  () => {
+    emoji_text2.textContent = "📋🧐💭";
+    emoji_text2.classList.remove("fade-out");
+  },
+  1000
+);
+
+button_2.addEventListener(
+  "mouseout",
+  () => {
+    emoji_text2.textContent = "&nbsp;";
+    emoji_text2.classList.add("fade-out");
+  },
+  1000
+);
