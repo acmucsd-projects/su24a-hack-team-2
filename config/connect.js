54de2d5c0798719cc4235c1d18ddfad4791e1ec3
3dcantaloupe <66147967+3dcantaloupe@users.noreply.github.com>
2024-08-28 17:16:50 -0700

    🗼 Set database when connecting

--- config/connect.js
+++ config/connect.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
  
 
 mongoose.connect(config.DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
     dbName: 'mainDB' }).then(() => {
   console.log('Connected to MongoDB database');
 });
